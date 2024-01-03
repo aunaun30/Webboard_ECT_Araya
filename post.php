9b4940d69de5d2904cec3faff3fe7d59a3b1417c
aunaun30 <152370449+aunaun30@users.noreply.github.com>
2024-01-03 13:49:26 +0700

    Create post.php
    Create post.php

---
+++ post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard GG</h1>
+    <hr>
+    <div align="center">
+        ต้องการดูกระทู้หมายเลข 3<br>
+    </div>
+    <br>
+<table style="border:2px solid black; width:40%" align="center">
+        <tr><td colspan="2" style="background-color: #6cd2fe;">แสดงความคิดเห็น</td></tr>
+        <tr>
+            <td><textarea style ="width: 99%"name="message" cols="40" rows="10"></textarea>
+            <br>
+            <center>
+            <input type="submit" value="ส่งข้อความ"></td>
+            </center>
+        </tr>
+</table>
+    <br>
+    <center>
+    <a href="index.html">กลับไปหน้าหลัก</a>
+    </center>
+</body>
+</html>
