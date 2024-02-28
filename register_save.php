50db782e17b46829cef77f39b105059894225106
aunaun30 <s6503052412153@email.kmutnb.ac.th>
2024-02-28 16:57:18 +0700

    Work9 (Succeed)
    Work9 (Succeed)

--- register_save.php
+++ register_save.php
@@ -1,14 +1,28 @@
 <?php 
+session_start();
+if(!isset($_POST['login'])){
+    header("location:index.php");
+    die();
+}
+
 $login=$_POST['login'];
-$passwd=$_POST['pwd'];
+$passwd=sha1($_POST['pwd']);
 $name=$_POST['name'];
 $gender=$_POST['gender'];
 $email=$_POST['email'];
 
 $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-
-$sql="INSERT INTO user (login, password, name, gender, email, role)
- VALUES ('$login', '$passwd', '$name', '$gender', '$email', 'm')";
- $conn->exec($sql);
- $conn=null;
+$sql="SELECT * FROM user where login='$login'";
+$result=$conn->query($sql);
+if($result->rowCount()==1){
+    $_SESSION['add_login']="error";
+}else{
+    $sql1="INSERT INTO user (login, password, name, gender, email, role)
+    VALUES ('$login', '$passwd', '$name', '$gender', '$email', 'm')";
+    $conn->exec($sql1);
+    $_SESSION['add_login']="success";
+}
+$conn=null;
+header("location:register.php");
+die();
 ?>
--- verify.php
+++ verify.php
@@ -4,30 +4,23 @@ if (isset($_SESSION['id'])){
     header("location: index.php");
     die();
 }
-?>
-
-    <?php
     $login=$_POST['login'];
-    $pwd=$_POST['pwd'];
-    if ($login=="admin" && $pwd=="ad1234"){
-        $_SESSION['username']='admin';
-        $_SESSION['role']='a';
-        $_SESSION['id']=session_id();
-        //echo "ยินดีต้อนรับคุณ ADMIN <br>";        
+    $pwd=($_POST['pwd']);
+    $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    $sql="SELECT * FROM user where login='$login' and password=sha1('$pwd')";
+    $result=$conn->query($sql);
+    if ($result->rowCount()==1){
+        $data=$result->fetch(PDO::FETCH_ASSOC);
+        $_SESSION["username"]=$data["login"];
+        $_SESSION["role"]=$data["role"];
+        $_SESSION["user_id"]=$data["id"];
+        $_SESSION["id"]=session_id();
         header("location:index.php");
-        die();
-    }elseif ($login=="member" && $pwd=="mem1234"){
-        $_SESSION['username']='member';
-        $_SESSION['role']='m';
-        $_SESSION['id']=session_id();
-        //echo "ยินดีต้อนรับคุณ MEMBER <br>";
-        header("location:index.php");
-        die();
+        die();      
     }else{
-       // echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง <br>";
-       $_SESSION['error']="error";
-       header("location:login.php");
-       die();
+        $_SESSION["error"]="error";
+        header("location:login.php");
+        die();
     }
+    $conn=null;
     ?>
-    
